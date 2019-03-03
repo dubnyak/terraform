b4df474f9a7083423d10ce3551248839d998dc53
dubnyak <46828141+dubnyak@users.noreply.github.com>
2019-03-03 23:06:50 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -v
         apt-get update -y
         apt-get install -y docker.io
+        apt-get install -y git
+        git clone https://github.com/dubnyak/new.git
+
         docker run -p 80:80 dubnyak/webserver
